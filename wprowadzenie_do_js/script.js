b9cd3f32d319e474e6f12948d23a9734b6f57122
MartaSzuran <marta.szuran@gmail.com>
2022-05-11 14:38:49 +0200

    First_chapter

---
+++ wprowadzenie_do_js/script.js
@@ -0,0 +1,46 @@
+const name = 'Lisa';
+
+console.log(name);
+
+let age = 23;
+
+age = 43 ;
+console.log(age);
+
+const ulubiony_film = 'piąty element';
+// console.log(ulubiony_film);
+
+let flower = 23;
+let insect = "fly";
+
+// console.log(flower, insect);
+
+const name_1 = "Majek";
+const favMeal = "schabowy";
+
+// console.log('Cześć, jestem' + name_1 + " i moje ulubione danie to" + favMeal);
+
+
+console.log(`Cześć, jestem ${name_1} i moje ulubione danie to ${favMeal}`);
+
+let myName = 'Marta';
+let myAnimalName = "Em";
+
+let animalAge = 11;
+console.log(`Cześć, jestem ${myName}, a to jest mój zwierzak o imieniu ${myAnimalName}.`)
+console.log(myAnimalName + "is " +  animalAge + " years old.");
+
+
+const firstName = 'Tomek'
+let age1 = 12
+age1 = 18
+ 
+let favColor = 'niebieski'
+const fav_meal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+let firstName2 = 'Ania'
+let age2 = 24
+let faovoriteColor = 'czerwony'
